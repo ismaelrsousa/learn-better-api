746153ba06746b7633b2eaf2c99bfc33ad672705
ismaelrsousa <ismaelsousa136@hotmail.com>
2021-10-19 17:56:23 -0300

    Fix

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/", user);
+app.use("/users", user);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
